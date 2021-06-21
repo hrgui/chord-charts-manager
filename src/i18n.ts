2254233f77ff266034b57e89ff7c2a7c8b713137
Harman Goei <5001432+hrgui@users.noreply.github.com>
2021-06-20 22:16:31 -0700

    refactor: upgrade i18next and react deps

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import Backend from "i18next-xhr-backend";
+import Backend from "i18next-http-backend";
 // TODO: UNCOMMENT when we actually do have other languages needed to be supported
 // import LanguageDetector from "i18next-browser-languagedetector";
 
